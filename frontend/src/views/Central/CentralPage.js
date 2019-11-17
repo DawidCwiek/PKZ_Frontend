fa536be3c40b747f3f85a48b30938bbf50e88757
dawidcwiek <cwiekdawid1@gmail.com>
2019-11-17 14:35:51 +0100

    init central page

---
+++ frontend/src/views/Central/CentralPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CentralPage = () => {
+  return (
+    <>
+      <h1>Strona Centrali</h1>
+      <h1>W budowie</h1>
+    </>
+  );
+};
+
+export default CentralPage;
