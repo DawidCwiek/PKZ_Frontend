26b75ee0d68dd4f32913c04fde97737ae3e86ec6
tomasz.kucharczyk <tomasz.kucharczyk@it-admin.pl>
2019-11-24 15:13:58 +0100

    Initial store list

--- frontend/src/components/organisms/StoreList.js
+++ frontend/src/components/organisms/StoreList.js
@@ -1,29 +1,76 @@
 import React from 'react';
+import Griddle, { RowDefinition, ColumnDefinition, plugins } from 'griddle-react';
 import ListWraper from 'components/atoms/ListWraper';
 
 const initialStores = [
   {
     id: 1,
-    name: 'store 1',
+    name: 'Zahir 1',
+    place: 'Lodz',
+    size: 'big',
   },
   {
     id: 2,
-    name: 'store 2',
+    name: 'Zahir 2',
+    place: 'Lodz',
+    size: 'big',
   },
   {
     id: 3,
-    name: 'store 3',
+    name: 'Zahir 3',
+    place: 'Lodz',
+    size: 'small',
+  },
+  {
+    id: 4,
+    name: 'Zahir 4',
+    place: 'Warszawa',
+    size: 'small',
   },
 ];
 
-const StoreList = () =>
-  initialStores.map(store => (
-    <ListWraper dupa>
-      <li>
-        <h1>{store.id}</h1>
-        <h1>{store.name}</h1>
-      </li>
-    </ListWraper>
-  ));
+const NewLayout = ({ Table, Pagination, Filter, SettingsWrapper }) => (
+  <div>
+    <Filter />
+    <Table />
+    <Pagination />
+    <SettingsWrapper />
+  </div>
+);
+
+const styleConfig = {
+  icons: {
+    TableHeadingCell: {
+      sortDescendingIcon: <small>▼</small>,
+      sortAscendingIcon: <small>▲</small>,
+    },
+  },
+  classNames: {
+    Row: 'row-class',
+  },
+  styles: {
+    Filter: { fontSize: 20 },
+    Table: { border: '2px solid #555 ', fontSize: 20, color: '#8884d8' },
+  },
+};
+
+const StoreList = () => (
+  <ListWraper>
+    <h1>Lista sklepów</h1>
+    <Griddle
+      plugins={[plugins.LocalPlugin]}
+      styleConfig={styleConfig}
+      data={initialStores}
+      components={{ Layout: NewLayout }}
+    >
+      <RowDefinition>
+        <ColumnDefinition id="id" title="Numer" />
+        <ColumnDefinition id="name" title="Nazwa" />
+        <ColumnDefinition id="place" title="Miasto" />
+        <ColumnDefinition id="size" title="Rozmiar" />
+      </RowDefinition>
+    </Griddle>
+  </ListWraper>
+);
 
 export default StoreList;
