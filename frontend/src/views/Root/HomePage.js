17d578f4009c491a1939da574a7bd26af9ab4712
dawidcwiek <cwiekdawid1@gmail.com>
2020-01-05 14:24:54 +0100

    [fix] component name

--- frontend/src/views/Root/HomePage.js
+++ frontend/src/views/Root/HomePage.js
@@ -6,7 +6,7 @@ const SignInButton = styled(NavLink)`
   width: 100px;
   height: 100px;
 `;
-const StoreTemplate = () => (
+const HomePage = () => (
   <>
     <h1>Home page</h1>
     <SignInButton as={NavLink} exact to="/login ">
@@ -15,4 +15,4 @@ const StoreTemplate = () => (
   </>
 );
 
-export default StoreTemplate;
+export default HomePage;
